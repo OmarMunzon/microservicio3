5daab9c82e2169730d2a883f8b38c9e32b59c3f3
OmarMunzon <75345035+OmarMunzon@users.noreply.github.com>
2025-11-09 16:49:04 -0400

    actualizado

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.graphql.schema import schema
 
 # App FastAPI
 app = FastAPI(title="API con GraphQL y Firestore")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],    
+)
 
 # Crear router GraphQL
 graphql_app = GraphQLRouter(schema)
